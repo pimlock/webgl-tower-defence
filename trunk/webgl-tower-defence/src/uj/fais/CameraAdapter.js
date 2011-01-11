183fca6bd3bf04f3d9860c9ad81a16b0da3ca65e
jakubowski.bartosz <jakubowski.bartosz@a9aff26e-dcc3-acb9-743d-093bd88c7c35>
2011-01-11 21:16:12 +0000

    obsługa poruszania klawiaturą

---
+++ trunk/webgl-tower-defence/src/uj/fais/CameraAdapter.js
@@ -0,0 +1,76 @@
+dojo.provide('uj.fais.CameraAdapter');
+
+uj.fais.CameraAdapter = function(_gameScene) {
+    var camera, xVector, yVector, trans, gameScene;
+
+    var _this = this;
+
+    this.prepare = function () {
+        var mat=camera.getRotMatrix();
+        trans=GLGE.mulMat4Vec4(mat,[0,0,-1,1]);
+        var mag=Math.pow(Math.pow(trans[0],2)+Math.pow(trans[1],2),0.5);
+        trans[0]=trans[0]/mag;
+        trans[1]=trans[1]/mag;
+        xVector = 0;
+        yVector = 0;
+    };
+
+    var init = function(_gameScene) {
+        gameScene = _gameScene;
+        camera = gameScene.getCamera();
+
+        _this.prepare();
+    };
+
+    init(_gameScene);
+
+    this.handleCameraMove = function() {
+        var cameraPosition = camera.getPosition();
+        if(xVector!=0 || yVector!=0){
+            var origin=[cameraPosition.x,cameraPosition.y,cameraPosition.z];
+
+            var dist2=gameScene.ray(origin,[-xVector,0,0]);
+            var dist3=gameScene.ray(origin,[0,-yVector,0]);
+            if (dist2.distance<5)
+                xVector=0;
+            if (dist3.distance<5)
+                yVector=0;
+            if (xVector!=0 || yVector!=0) {
+                camera.setLocY(cameraPosition.y+yVector*0.05);
+                camera.setLocX(cameraPosition.x+xVector*0.05);
+            }
+	    }
+    };
+
+    this.rotateX = function(angle) {
+
+    };
+
+    this.rotateY = function(angle) {
+
+    };
+
+    this.rotateZ = function(angle) {
+
+    };
+
+    this.moveForward = function() {
+        yVector=yVector+parseFloat(trans[1]);
+        xVector=xVector+parseFloat(trans[0]);
+    };
+
+    this.moveBackward = function() {
+        yVector=yVector-parseFloat(trans[1]);
+        xVector=xVector-parseFloat(trans[0]);
+    };
+
+    this.moveLeft = function() {
+        yVector=yVector+parseFloat(trans[0]);
+        xVector=xVector-parseFloat(trans[1]);
+    };
+
+    this.moveRight = function() {
+        yVector=yVector-parseFloat(trans[0]);
+        xVector=xVector+parseFloat(trans[1]);
+    };
+};
--- trunk/webgl-tower-defence/src/uj/fais/Setup.js
+++ trunk/webgl-tower-defence/src/uj/fais/Setup.js
@@ -1,17 +1,21 @@
 dojo.provide("uj.fais.Setup");
 
+dojo.require('uj.fais.KeyboardAdapter');
+dojo.require('uj.fais.CameraAdapter');
+
 uj.fais.Setup = function(canvasId) {
     /* private member declaration */
-    var doc, gameRenderer, gameScene, cameraPosition;
+    var doc, gameRenderer, gameScene, cameraPosition, cameraAdapter, keyboardAdapter;
 
     /* private methods */
     var render = function() {
+        keyboardAdapter.handleInput(cameraAdapter);
         gameRenderer.render();
     };
 
     var init = function(canvasId) {
         gameRenderer = new GLGE.Renderer(document.getElementById(canvasId));
-        
+
         doc = new GLGE.Document();
         doc.load('src/uj/fais/board.xml');
     };
@@ -22,6 +26,9 @@ uj.fais.Setup = function(canvasId) {
         gameScene = doc.getElement('mainscene');
         
         gameRenderer.setScene(gameScene);
+
+        cameraAdapter = new uj.fais.CameraAdapter(gameScene);
+        keyboardAdapter = new uj.fais.KeyboardAdapter();
 
         cameraPosition = gameScene.camera.getPosition();
 
