77e9a696a9503cbeb9342b025368212e288075e8
jakubowski.bartosz <jakubowski.bartosz@a9aff26e-dcc3-acb9-743d-093bd88c7c35>
2011-01-17 21:28:57 +0000

    Pomykajacy 'Potworek'

--- trunk/webgl-tower-defence/src/uj/fais/Monster.js
+++ trunk/webgl-tower-defence/src/uj/fais/Monster.js
@@ -2,12 +2,35 @@ dojo.provide('uj.fais.Monster');
 
 dojo.require('uj.fais');
 
-uj.fais.Monster = function() {
+uj.fais.Monster = function(_monsterMesh, _monsterMaterial) {
     this.health = 0;
     this.position = new uj.fais.Position(0, 0);
+    var monsterObject = null;
+    var dt = 0.1;
 
-    this.putOnGameBoard = function() {
-        this.gameBoard    
+    var init = function(_monsterMesh, _monsterMaterial) {
+        var material = new GLGE.Material("mm1");
+        material.setColor('#0000ff');
+
+        monsterObject = new GLGE.Object("m1");
+        monsterObject.setMesh(_monsterMesh);
+        monsterObject.setMaterial(material);
+    };
+
+    init(_monsterMesh, null);
+
+    this.putOnGameBoard = function(_pos, _gameScene) {
+        monsterObject.setLocX(_pos[0]);
+        monsterObject.setLocY(_pos[1]);
+        monsterObject.setLocZ(_pos[2]);
+
+        _gameScene.addChild(monsterObject);
+    };
+
+    this.move = function(_vector) {
+        monsterObject.setLocX(monsterObject.getLocX() + _vector[0] * dt);
+        monsterObject.setLocY(monsterObject.getLocY() + _vector[1] * dt);
+        monsterObject.setLocZ(monsterObject.getLocZ() + _vector[2] * dt);
     };
 
     this.wasHit = function(power) {
--- trunk/webgl-tower-defence/src/uj/fais/Setup.js
+++ trunk/webgl-tower-defence/src/uj/fais/Setup.js
@@ -5,16 +5,21 @@ dojo.require('uj.fais.CameraAdapter');
 dojo.require('uj.fais.MouseAdapter');
 dojo.require('uj.fais.SceneObjectPicker');
 
+dojo.require('uj.fais.Monster');
+
 uj.fais.Setup = function(canvasId) {
     /* private member declaration */
     var doc, viewElement, gameRenderer, gameScene, cameraAdapter, keyboardAdapter, mouseAdapter, objectPicker;
 
+    var monster1;
     /* private methods */
     var gameLoop = function() {
         var mouseRelativePosition = mouseAdapter.getMouseRelativePosition();
         objectPicker.highlight(mouseRelativePosition);
 
         keyboardAdapter.handleInput(cameraAdapter);
+
+        monster1.move([1,0,0]);
         gameRenderer.render();
     };
 
@@ -31,13 +36,16 @@ uj.fais.Setup = function(canvasId) {
 
     doc.onLoad = function(canvasId) {
         gameScene = doc.getElement('mainscene');
-        
+
         gameRenderer.setScene(gameScene);
 
         cameraAdapter = new uj.fais.CameraAdapter(gameScene);
         keyboardAdapter = new uj.fais.KeyboardAdapter();
 
         objectPicker = new uj.fais.SceneObjectPicker(doc.getElement("yellow"), gameScene, doc.getElement('cube2'));
+
+        monster1 = new uj.fais.Monster(doc.getElement('cube2'), null);
+        monster1.putOnGameBoard([-10, -6, 2], gameScene);
 
         viewElement.onmouseover = function(e) { mouseAdapter.setMouseActive(); };
         viewElement.onmouseout = function(e) { mouseAdapter.setMouseInActive(); };
